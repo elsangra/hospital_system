ccf99dacc03ba9bf1b5b7a3a5627b1919cce7620
elsangra <koweros100@gmail.com>
2024-01-30 16:31:25 +0300

    Initial commit.

---
+++ src/patient_management_frontend/src/index.js
@@ -0,0 +1,19 @@
+import { patient_management_backend } from "../../declarations/patient_management_backend";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await patient_management_backend.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
